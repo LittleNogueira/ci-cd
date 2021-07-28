304bccc022edef906cba01ac7cae5a282709e020
Mateus Nogueira de Oliveira <mateus_nogol@outlook.com>
2021-07-28 14:04:15 +0000

    Little nogueira main patch 04343

---
+++ main.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  version = "~> 3.0"
+  region  = "us-east-1"
+}
+
+resource "aws_instance" "application-ci-cd" {
+  ami           = "ami-0c2b8ca1dad447f8a"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "application-ci-cd"
+  }
+}
+
+resource "local_file" "ip-file" {
+    content  = aws_instance.application-ci-cd.public_ip
+    filename = "ip.txt"
+}
