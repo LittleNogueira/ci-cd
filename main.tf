071222ee7f236fe7b2e024757f69953f88304b14
Mateus Nogueira <mateus_nogol@outlook.com>
2021-07-29 23:10:12 -0300

    Especifica o par de chaves

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ provider "aws" {
 resource "aws_instance" "application-ci-cd" {
   ami           = "ami-0c2b8ca1dad447f8a"
   instance_type = "t2.micro"
+  key_name      = "gitlab-ssh"
 
   tags = {
     Name = "application-ci-cd"
