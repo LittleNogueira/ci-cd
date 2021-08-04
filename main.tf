5e0261f0982ea84b0ea49fc49a0a5f8d4a885837
Mateus Nogueira de Oliveira <mateus_nogol@outlook.com>
2021-08-04 21:22:06 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -32,6 +32,15 @@ resource "aws_security_group" "gitlab-security-group" {
     ]
   }
 
+  ingress {
+    from_port        = 8000
+    to_port          = 8000
+    protocol         = "http"
+    cidr_blocks = [
+      "0.0.0.0/0"
+    ]
+  }
+
   egress {
     from_port        = 0
     to_port          = 0
